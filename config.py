a3e8336b4080a2a07c4e3c9539a799d0f94e2299
eslam11122 <69135763+eslam11122@users.noreply.github.com>
2021-10-29 21:19:02 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-emailID = '' #Enter your emailID here
-password = '' #Enter yor discord password
-bot_link = '' #Enter bot name you wish to vote
+emailID = 'alshaslam11@gmail.com' #Enter your emailID here
+password = 'Eslam Wael11' #Enter yor discord password
+bot_link = 'https://top.gg/bot/282859044593598464' #Enter bot name you wish to vote
